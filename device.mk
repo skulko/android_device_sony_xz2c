8ba1592eea24eff743a02e69e5242203d2b285e7
LS <23030592+skulko@users.noreply.github.com>
2024-06-10 18:09:33 +0200

    bring up microg etc.
    Change-Id: Id9f8c410b4b2ba56640791731a135e7b60e2ec37

--- device.mk
+++ device.mk
@@ -53,3 +53,13 @@ PRODUCT_COPY_FILES += \
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
+# Local app overlay
+PRODUCT_PACKAGES += \
+   AuroraServices \
+   FakeStore \
+   GmsCore \
+   GsfProxy \
+   SimpleKeyboard \
+   SimpleSMS
+
